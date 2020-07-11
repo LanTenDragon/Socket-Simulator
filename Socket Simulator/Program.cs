ad656ff842110e19494eaec1512b0e103c8763a0
LanTenDragon <pantheonia1@gmail.com>
2020-07-12 00:07:57 +0800

    Add mongo models

--- Socket Simulator/Program.cs
+++ Socket Simulator/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Socket_Simulator
 {
     static class Program
     {
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +16,49 @@ namespace Socket_Simulator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            User user = new User();
+            Mongo mongo = new Mongo();
+            UserId userid = new UserId();
+
+            DialogResult dialogResult;
+
+            using (var loginForm = new LoginForm(user, mongo, userid))
+                dialogResult = loginForm.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                Application.Run(new Form1(mongo, userid));
+            }
         }
+    }
+
+    public class Mongo
+    {
+       public MongoClient dbClient = new MongoClient("mongodb://lanten:PantheonIA1@lantendragon.southeastasia.cloudapp.azure.com/IoT?authSource=admin");
+    }
+
+    public class User
+    {
+        public ObjectId _id { get; set; }
+        public string username { get; set; }
+        public string hash { get; set; }
+        public int __v { get; set; }
+    }
+
+    public class UserId
+    {
+        public string userid { get; set; }
+    }
+
+    public class Socket
+    {
+        public ObjectId _id { get; set; }
+        public bool status { get; set; }
+        public string image { get; set; }
+        public ObjectId[] groups { get; set; }
+        public string name { get; set; }
+        public ObjectId belongsTo { get; set; }
+        public string colour { get; set; }
+        public int __v { get; set; }
     }
 }
